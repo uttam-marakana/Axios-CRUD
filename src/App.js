1c7f3c3a02972fe521ece049b1fecabb6b3a1103
uttam-marakana <uttammarakana03@email.com>
2026-02-06 12:46:53 +0530

    added new file and code content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-// import "./App.css";
 import Create from "./crud_oper/Create";
 import Read from "./crud_oper/Read";
+import Update from "./crud_oper/Update";
+import Layout from "./layout/Layout";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Update from "./crud_oper/Update";
 
 function App() {
   return (
-    <div className="container">
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Create />} />
-          <Route exact path="/read" element={<Read />} />
-          <Route exact path="/update" element={<Update />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Create />} />
+          <Route path="read" element={<Read />} />
+          <Route path="update" element={<Update />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
